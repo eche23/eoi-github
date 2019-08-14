97e7981ca0cc0c25a012ebbd11ead3eb3edd97aa
Airán Vega Rodríguez <airan_234@hotmail.com>
2019-08-14 20:51:37 +0100

    Peticion de todos los commits

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Commits msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Commits from './components/Commits.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Commits
   }
 }
 </script>
