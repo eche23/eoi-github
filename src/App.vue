727bcefdd8ea7b2531c4fd3196290f068c22a356
eche23 <eche2312@gmail.com>
2019-08-14 20:53:30 +0100

    created component commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <Commit/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Commit from './components/Commit.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Commit
   }
 }
 </script>
---
+++ src/components/Commit.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="commit">
+    
+    <p>
+      <a :href="commit.html_url" class="link">{{commit.committer.id}}</a> {{commit.message}} <br>
+      by: <a :href="commit.author.hmlt_url">{{commit.author.name}}</a> at {{commit.committer.date}} 
+    </p>
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Commit',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
